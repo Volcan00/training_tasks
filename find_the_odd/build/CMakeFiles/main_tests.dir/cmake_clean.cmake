bbbaa43d94fc046009e1a997080623736c1d3372
dany <dyagnichenko@gmail.com>
2024-09-20 10:47:02 +0200

    Add function that retuns a number that appears an odd number of times in a given array. Add basic unit tests. Add CMake.

---
+++ find_the_odd/build/CMakeFiles/main_tests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main_tests.dir/tests/main_tests.cpp.o"
+  "CMakeFiles/main_tests.dir/tests/main_tests.cpp.o.d"
+  "main_tests"
+  "main_tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
